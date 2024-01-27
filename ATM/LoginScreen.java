a992ec67f6b1827ae983d8367a87386404630ee8
huydduong05 <huydduong05@gmail.com>
2024-01-27 02:22:21 -0800

    created screens
    screens dont have accounts linked to them yet

--- ATM/LoginScreen.java
+++ ATM/LoginScreen.java
@@ -44,6 +44,7 @@ public class LoginScreen extends JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
 
+        setTitle("ATM Machine");
         jDesktopPane1 = new javax.swing.JDesktopPane();
         jPanel1 = new javax.swing.JPanel();
         ATMLogo = new javax.swing.JPanel();
@@ -226,14 +227,14 @@ public class LoginScreen extends JFrame {
     }                                           
 
     private void hideLogin() { 
+        new MainScreen().setVisible(true);
         dispose();
-        System.exit(0);
     }                                     
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public void start() {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
